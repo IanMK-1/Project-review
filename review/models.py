5fcea001cc702d68a6a90d490bedbc71d789af73
ian mwangi <iankimotho233@gmail.com>
2020-03-14 11:33:47 +0300

    Create save, delete and update_bio methods for Profile class

--- review/models.py
+++ review/models.py
@@ -10,6 +10,20 @@ class Profile(models.Model):
     phone_no = models.CharField(max_length=20, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def update_bio(cls, id, new_bio):
+        cls.objects.filter(id=id).update(bio=new_bio)
+        updated_bio = cls.objects.get(id=id)
+        return updated_bio
 
 class Project(models.Model):
     title = models.CharField(max_length=30)
@@ -18,6 +32,9 @@ class Project(models.Model):
     live_link = models.CharField(max_length=50)
     user_project = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 
 class Rating(models.Model):
     design = models.PositiveIntegerField(max_length=10)
@@ -25,3 +42,6 @@ class Rating(models.Model):
     content = models.PositiveIntegerField(max_length=10)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user_rating = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.design
