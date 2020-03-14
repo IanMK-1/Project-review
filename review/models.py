ff4407defdc60d32359d9cb53bd9fd4474580057
ian mwangi <iankimotho233@gmail.com>
2020-03-14 11:10:00 +0300

    Create the Profile model class

--- review/models.py
+++ review/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = CloudinaryField('image', blank=True)
+    bio = models.TextField(blank=True)
+    phone_no = models.CharField(max_length=20, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
