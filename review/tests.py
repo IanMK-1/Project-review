6d02a6f622c25a2ef421d100a0407ea93aef101c
ian mwangi <iankimotho233@gmail.com>
2020-03-14 13:15:23 +0300

    Define save, delete, update design, content and usability rating tests for Rating class

--- review/tests.py
+++ review/tests.py
@@ -111,4 +111,61 @@ class ProjectClassTest(TestCase):
         self.assertTrue(updated_project.image != self.new_project.image)
 
 
+class RatingTestClass(TestCase):
+    def setUp(self) -> None:
+        # creating a new user and saving it
+        self.new_user = User(password='weareone', username='ian', first_name='ian', last_name='mark',
+                             email='imk@gmail.com')
+        self.new_user.save()
 
+        # creating a new profile and saving it
+        self.new_profile = Profile(profile_pic='imk.jpg', bio='hello', phone_no='0711223344', user=self.new_user)
+        self.new_profile.save()
+
+        # creating a new project and saving it
+        self.new_project = Project(title='instagram', image='insta.jpg', description='instagram app',
+                                   live_link='instagram.com',
+                                   user_project=self.new_user)
+        self.new_project.save()
+
+        # creating a new rating for project and saving it
+        self.new_rating = Rating(design=1, usability=6, content=8, project=self.new_project, user_rating=self.new_user)
+        self.new_rating.save()
+
+    def tearDown(self) -> None:
+        User.objects.all().delete()
+        Profile.objects.all().delete()
+        Project.objects.all().delete()
+        Rating.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile, Profile))
+        self.assertTrue(isinstance(self.new_user, User))
+        self.assertTrue(isinstance(self.new_project, Project))
+        self.assertTrue(isinstance(self.new_rating, Rating))
+
+    def test_save_rating(self):
+        self.new_rating.save_rating()
+        ratings = Rating.objects.all()
+        self.assertTrue(len(ratings) > 0)
+
+    def test_delete_rating(self):
+        self.new_rating.save_rating()
+        self.new_rating.delete_rating()
+        ratings = Rating.objects.all()
+        self.assertTrue(len(ratings) == 0)
+
+    def test_update_design_rating(self):
+        self.new_rating.save_rating()
+        updated_rating = Rating.update_design_rating(self.new_rating.id, 5)
+        self.assertEqual(updated_rating.design, 5)
+
+    def test_update_usability_rating(self):
+        self.new_rating.save_rating()
+        updated_rating = Rating.update_usability_rating(self.new_rating.id, 9)
+        self.assertEqual(updated_rating.usability, 9)
+
+    def test_update_content_rating(self):
+        self.new_rating.save_rating()
+        updated_rating = Rating.update_content_rating(self.new_rating.id, 2)
+        self.assertEqual(updated_rating.content, 2)
