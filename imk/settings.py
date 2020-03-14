08f7983f1f712e03f7c87e72a5b0acacc3ba000f
ian mwangi <iankimotho233@gmail.com>
2020-03-14 10:59:28 +0300

    Configured cloudinary for image upload to settings.py file

--- imk/settings.py
+++ imk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import cloudinary
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,8 +29,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+cloudinary.config(
+    cloud_name="dg9gqlrva",
+    api_key="957948429853469",
+    api_secret="XvUp2xjKj8RWOJH8t23EjkCbce4"
+)
+
 INSTALLED_APPS = [
     'review',
+    'cloudinary',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
