4adfe65b30b57a83d52b8b5c6b0b1b43adc34b2d
ian mwangi <iankimotho233@gmail.com>
2020-03-14 18:00:35 +0300

    Add profile.html page and profile view function

--- review/views.py
+++ review/views.py
@@ -9,3 +9,17 @@ from .models import Profile, Project, Rating
 def home(request):
     all_projects = Project.objects.all()
     return render(request, 'home.html', {"all_projects": all_projects})
+
+
+def profile(request):
+    current_user = request.user
+    try:
+        user_profile = Profile.objects.get(user=current_user)
+        user_projects = Project.objects.filter(user_project=current_user)
+    except ObjectDoesNotExist:
+        user_profile = Profile(user=current_user)
+        user_profile.save_profile()
+        user_profile = Profile.objects.get(user=current_user)
+        user_projects = Project.objects.filter(user_project=current_user)
+
+    return render(request, 'profile.html', {"user_profile": user_profile, "user_projects": user_projects})
