9061274ab82a14eff2e207769a15233445861849
ian mwangi <iankimotho233@gmail.com>
2020-03-15 04:31:37 +0300

    Modified serializer field to image field to show image link

--- review/serializer.py
+++ review/serializer.py
@@ -1,19 +1,18 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 
 class ProfileSerializer(serializers.Serializer):
     user_username = serializers.ReadOnlyField(source='user.username')
     bio = serializers.CharField(max_length=500)
     phone_no = serializers.CharField(max_length=20)
-    profile_pic = CloudinaryField('image')
+    profile_pic = serializers.ImageField()
 
 
 class ProjectSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=30)
     description = serializers.CharField(max_length=500)
     live_link = serializers.CharField(max_length=20)
+    image = serializers.ImageField()
     posted_on = serializers.DateTimeField()
     user_project_username = serializers.ReadOnlyField(source='user_project.username')
     user_project_email = serializers.ReadOnlyField(source='user_project.email')
